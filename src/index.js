347b8029a35d2d58d068cc5603ab20a108ecb515
garizs <payka04@hotmail.com>
2024-04-05 22:29:54 +0400

    hotfix after moving

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
-import autoprefix from 'autoprefixer';
+import auto_prefix_with_styles from 'autoprefixer-css-js';
+import postcss from 'postcss';
 
-function auto_prefix_with_styles(styles) {
-    const prefixedStyles = {};
+function convertStylesToCSS(styles) {
+    const cssString = Object.keys(styles)
+        .map(property => {
+            const value = styles[property];
+            return `${property}: ${value};`;
+        })
+        .join(' ');
 
-    // Apply autoprefixer to each style property
-    Object.keys(styles).forEach(key => {
-        const value = styles[key];
-        prefixedStyles[key] = autoprefix.process(value).css;
-    });
-
-    return prefixedStyles;
+    return cssString;
 }
-export default auto_prefix_with_styles;
+
+function styledPrefixer(styles) {
+    const cssString = convertStylesToCSS(styles);
+    const processedCss = postcss([auto_prefix_with_styles]).process(cssString).css;
+    return processedCss;
+}
+
+export default styledPrefixer;
+
